9177e7a8cceb7090d0629becb81c2abb5b345885
gs20978 <s20978@pjwstk.edu.pl>
2021-07-08 01:38:58 +0200

    project commit

---
+++ DoctorAPI/Migrations/20210509101449_AddedSeeders.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class AddedSeeders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "email1", "Doctor1", "1" },
+                    { 2, "email1", "Doctor2", "2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicaments",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "desc1", "Medicament1", "type1" },
+                    { 2, "desc2", "Medicament2", "type1" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "Birthdate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 5, 9, 12, 14, 48, 797, DateTimeKind.Local).AddTicks(170), "Patient1", "1" },
+                    { 2, new DateTime(2021, 5, 9, 12, 14, 48, 806, DateTimeKind.Local).AddTicks(2268), "Patient2", "2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescription",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateTime(2021, 5, 9, 12, 14, 48, 826, DateTimeKind.Local).AddTicks(6432), new DateTime(2021, 5, 9, 12, 14, 48, 826, DateTimeKind.Local).AddTicks(7033), 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 2, new DateTime(2021, 5, 9, 12, 14, 48, 826, DateTimeKind.Local).AddTicks(8413), new DateTime(2021, 5, 9, 12, 14, 48, 826, DateTimeKind.Local).AddTicks(8426), 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 1, 1, "det1", null });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 2, 2, "det1", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "IdDoctor",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "IdDoctor",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 2);
+        }
+    }
+}
